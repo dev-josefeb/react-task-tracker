9f644aeb91c27e5e9ae017bcf231a3350ef2de5a
dev-josefeb <febinjose@protonmail.com>
2021-06-05 18:17:04 +0200

    Add basic styling and the button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
-import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button.js";
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add"></Button>
     </header>
   );
 };
 
 Header.defaultProps = {
   title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
