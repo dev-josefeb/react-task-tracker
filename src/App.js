786ee4786fca1afe5400b2609371b48aebbe48c7
dev-josefeb <febinjose@protonmail.com>
2023-03-04 12:02:40 +0100

    Add new task to the global state

--- src/App.js
+++ src/App.js
@@ -26,7 +26,10 @@ function App() {
   ]);
 
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) +1 
+
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
   }
 
   const deleteTask = id => {
