d4a219f7e48548b975e0c95ed2c01c8033c3af2a
dev-josefeb <febinjose@protonmail.com>
2023-03-05 17:15:29 +0100

    Implement fetch and delete through API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddTask from './components/AddTask';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([]);
 
-  const [tasks, setTasks] = useState();
+  useEffect(()=>{
+   const getTasks = async () => {
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+   }
+    
+    getTasks()
+  }, [])
 
+  const fetchTasks = async() => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) +1 
@@ -16,8 +29,11 @@ function App() {
     setTasks([...tasks, newTask])
   }
 
-  const deleteTask = id => {
-    console.log('delete', id);
+  const deleteTask = async id => {
+    await fetch(
+      `http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter(task => task.id !== id));
   };
 
